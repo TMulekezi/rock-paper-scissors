8c8bcdd412991e8fdb80c1317ebac897c7e58302
TMulekezi <TMulekezi>
2022-12-01 19:16:31 +0000

    add logic and visuals

--- js/javascript.js
+++ js/javascript.js
@@ -62,14 +62,14 @@ function setImage(image, player) {
     if (actor.classList.contains("active")) {
         
         const img = document.querySelector(`.${player} .image`);
-        img.src = `images/${image}.jpeg`;
+        img.src = `images/${image}.gif`;
         img.alt = `image of ${image}`;
         return;
     }
     
     const img = document.createElement('img');
     img.classList.add("image");
-    img.src = `images/${image}.jpeg`;
+    img.src = `images/${image}.gif`;
     img.alt = `image of ${image}`;
     img.setAttribute("style", "height: 70px; width: 70px");
 
@@ -79,25 +79,68 @@ function setImage(image, player) {
 
 
 function play(e) {
+    const outcome = document.querySelector('.result');
+    if (gameOver) {
+        buttons.forEach(button => button.removeEventListener('click', play));
+        
+        const stage = document.querySelector(".stage");
+        while (stage.firstChild) {
+            stage.removeChild(stage.lastChild);
+        }
+        stage.textContent = "Click replay to play again";
+        stage.setAttribute("style", "justify-content: center; align-items: center; gap: 10px");
+        stage.appendChild(refresh);
+        return;
+    } 
+    
+
     let word = this.classList.value;
     const choices = word.split(" ");
     let playerChoice = choices[1];
     let computerChoice = getComputerChoice();
     setImage(computerChoice,"computer");
     setImage(playerChoice, "player");
-    incrementScore(playerScore, computerScore, playRound(playerChoice, computerChoice));
-}
-
-function incrementScore(playerScore, computerScore, result) {
+    
+    let result = playRound(playerChoice, computerChoice);
     if (result === 1) {
-
+        playerScore++;
     } else if (result === 2) {
-
+        computerScore++;
+    } else {
+        //do nothing
     }
+
+    const scoreboard = document.querySelector(".score-board");
+    scoreboard.textContent = `Score: ${playerScore} - ${computerScore}`;
+
+    
+    if (playerScore === 5) {
+        outcome.textContent = "You Win!!!";
+        gameOver = true;
+    } else if (computerScore === 5) {
+        outcome.textContent = "You Loose :(";
+        gameOver = true;
+    }
+    
 }
+
+
 
 let playerScore = 0;
 let computerScore = 0;
+let gameOver = false;
+
+
+
+
 const buttons = document.querySelectorAll('.button');
 buttons.forEach(button => button.addEventListener('click', play));
 
+const refresh = document.createElement("button");
+refresh.textContent = "Replay";
+refresh.style.width = "60px";
+refresh.style.height = "30px";
+
+refresh.addEventListener("click",  function() {
+    document.location.reload();
+})
